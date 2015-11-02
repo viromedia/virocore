f9cad6b8ddbf52f79c627e8ecf57c1353fd9f9e5
Raj Advani <radvani@viromedia.com>
2015-11-02 16:00:03 -0500

    Fix flipped UV coordinates

--- ViroRenderer/VROLayerSubstrate.cpp
+++ ViroRenderer/VROLayerSubstrate.cpp
@@ -19,7 +19,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[0].y = -y / 2;
     vertexLayout[0].z = z;
     vertexLayout[0].u = 0;
-    vertexLayout[0].v = 0;
+    vertexLayout[0].v = 1;
     vertexLayout[0].nx = 0;
     vertexLayout[0].ny = 0;
     vertexLayout[0].nz = -1;
@@ -28,7 +28,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[1].y = -y / 2;
     vertexLayout[1].z = z;
     vertexLayout[1].u = 1;
-    vertexLayout[1].v = 0;
+    vertexLayout[1].v = 1;
     vertexLayout[1].nx = 0;
     vertexLayout[1].ny = 0;
     vertexLayout[1].nz = -1;
@@ -37,7 +37,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[2].y =  y / 2;
     vertexLayout[2].z = z;
     vertexLayout[2].u = 0;
-    vertexLayout[2].v = 1;
+    vertexLayout[2].v = 0;
     vertexLayout[2].nx = 0;
     vertexLayout[2].ny = 0;
     vertexLayout[2].nz = -1;
@@ -46,7 +46,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[3].y = y / 2;
     vertexLayout[3].z = z;
     vertexLayout[3].u = 1;
-    vertexLayout[3].v = 1;
+    vertexLayout[3].v = 0;
     vertexLayout[3].nx = 0;
     vertexLayout[3].ny = 0;
     vertexLayout[3].nz = -1;
@@ -55,7 +55,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[4].y =  y / 2;
     vertexLayout[4].z = z;
     vertexLayout[4].u = 0;
-    vertexLayout[4].v = 1;
+    vertexLayout[4].v = 0;
     vertexLayout[4].nx = 0;
     vertexLayout[4].ny = 0;
     vertexLayout[4].nz = -1;
@@ -64,7 +64,7 @@ void VROLayerSubstrate::buildQuad(VROLayerVertexLayout *vertexLayout) {
     vertexLayout[5].y = -y / 2;
     vertexLayout[5].z = z;
     vertexLayout[5].u = 1;
-    vertexLayout[5].v = 0;
+    vertexLayout[5].v = 1;
     vertexLayout[5].nx = 0;
     vertexLayout[5].ny = 0;
     vertexLayout[5].nz = -1;
