41698e26e11a39e8e4213170c9cc0ea42dd71b91
Raj Advani <radvani@viromedia.com>
2015-11-11 16:18:29 -0800

    Replace Boba with Moments assets

--- ViroRenderer/GameViewController.m
+++ ViroRenderer/GameViewController.m
@@ -20,9 +20,8 @@
 {
     // view
     MTKView *_view;
-    
     VROScene *_scene;
-    VROView *_testView;
+    
 }
 
 - (void)setupRendererWithView:(MTKView *)view context:(VRORenderContext *)renderContext {
@@ -38,40 +37,36 @@
     
     size_t dataLength;
     int width, height;
-    void *data = VROImageLoadTextureDataRGBA8888("boba", &dataLength, &width, &height);
+    void *data = VROImageLoadTextureDataRGBA8888("momentslogo", &dataLength, &width, &height);
     
     std::shared_ptr<VROLayer> layerA = std::make_shared<VROLayer>(*context);
-    layerA->setFrame(VRORectMake(-1.0, 0, 2, 2.0, 2.0));
-    layerA->setBackgroundColor({ 1.0, 0.0, 0.0, 1.0 });
+    layerA->setFrame(VRORectMake(-1, -1, 10, 2.0, 2.0));
+    layerA->setBackgroundColor({ 1.0, 1.0, 1.0, 1.0 });
     layerA->setContents(data, dataLength, width, height);
     
-    std::shared_ptr<VROLayer> layerB = std::make_shared<VROLayer>(*context);
-    layerB->setFrame(VRORectMake(1.0, 1.0, 1.0, 1.0));
-    layerB->setBackgroundColor({ 0.0, 0.0, 1.0, 1.0 });
-    layerB->setContents(data, dataLength, width, height);
+    VROView *labelView = [[VROView alloc] initWithFrame:CGRectMake(0, 0, 100, 10) context:context];
+    labelView.vroLayer->setFrame(VRORectMake(0, 1.9, 2.0, 0.2));
     
-    _testView = [[VROView alloc] initWithFrame:CGRectMake(0, 0, 50, 50) context:context];
-    _testView.vroLayer->setFrame(VRORectMake(0, 0, 1.0, 1.0));
+    [labelView setBackgroundColor:[UIColor clearColor]];
     
-    [_testView setBackgroundColor:[UIColor clearColor]];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
-    [label setText:@"Test"];
+    UILabel *label = [[UILabel alloc] initWithFrame:labelView.bounds];
+    [label setText:@"Moments"];
     [label setBackgroundColor:[UIColor clearColor]];
+    [label setTextAlignment:NSTextAlignmentCenter];
     [label setTextColor:[UIColor whiteColor]];
-    [label setFont:[UIFont systemFontOfSize:16]];
+    [label setFont:[UIFont systemFontOfSize:14]];
     
-    [_testView addSubview:label];
-    [_testView update];
+    [labelView addSubview:label];
+    [labelView update];
     
     _scene->addLayer(layerA);
-    layerA->addSublayer(layerB);
-    layerB->addSublayer(_testView.vroLayer);
+    layerA->addSublayer(labelView.vroLayer);
     
     free (data);
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        layerA->setPosition({0.0, 0.0, 2.0});
+        layerA->setPosition({0, 0, 1});
+        VROAnimation::setAnimationDuration(2);
     });
 }
 
