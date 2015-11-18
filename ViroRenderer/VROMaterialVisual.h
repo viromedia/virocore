81cf3e1b184f77869eb5c0efebb424af71c55220
Raj Advani <radvani@viromedia.com>
2015-11-17 16:35:57 -0800

    Flesh out VROMaterialVisual data

--- ViroRenderer/VROMaterialVisual.h
+++ ViroRenderer/VROMaterialVisual.h
@@ -27,37 +27,83 @@ enum class VROFilterMode {
     Linear
 };
 
+enum class VROContentType {
+    Fixed,
+    Texture2D,
+    TextureCube
+};
+
 class VROMaterialVisual {
     
 public:
     
+    VROMaterialVisual() :
+        _contentType(VROContentType::Fixed),
+        _contentsColor({ 1.0, 1.0, 1.0 }),
+        _intensity(1.0),
+        _wrapS(VROWrapMode::Clamp),
+        _wrapT(VROWrapMode::Clamp),
+        _minificationFilter(VROFilterMode::Linear),
+        _magnificationFilter(VROFilterMode::Linear),
+        _mipFilter(VROFilterMode::None)
+    {}
+    
     void setContents(VROVector3f contents) {
         _contentsColor = contents;
+        _contentType = VROContentType::Fixed;
     }
+    
     void setContents(std::shared_ptr<VROTexture> texture) {
         _contentsTexture = texture;
+        _contentType = VROContentType::Texture2D;
     }
+    
     void setContents(std::vector<std::shared_ptr<VROTexture>> cubeTextures) {
         _contentsCube = cubeTextures;
+        _contentType = VROContentType::TextureCube;
     }
     
 private:
     
     /*
-     Only one of these can be populated.
+     Indicates the content type for this visual.
+     */
+    VROContentType _contentType;
+    
+    /*
+     If the visual is determined by a fixed color, _contentsColor is populated.
      */
     VROVector3f _contentsColor;
+    
+    /*
+     If the visual is determiend by a texture, this variable will be populated
+     with the texture.
+     */
     std::shared_ptr<VROTexture>  _contentsTexture;
+    
+    /*
+     If the visual is determiend by a set of 6 textures forming a cube map, this
+     variable will be populated.
+     */
     std::vector<std::shared_ptr<VROTexture>> _contentsCube;
     
+    /*
+     Modulates the impact of this visual on the overall material appearance.
+     */
     float _intensity;
     
+    /*
+     Transformation applied to the texture coordinates provided by the geometry object
+     the material is attached to.
+     */
     VROMatrix4f _contentsTransform;
     
+    /*
+     Standard texture mapping properties.
+     */
     VROWrapMode _wrapS, _wrapT;
     VROFilterMode _minificationFilter, _magnificationFilter, _mipFilter;
     
-    float _maxAnisotropy;
     VROVector3f _borderColor;
     
 };
