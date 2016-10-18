5054ef154544a64ccd50eb114ba2d0f2214c236a
Raj Advani <radvani@viromedia.com>
2016-10-18 18:39:35 -0400

    SIMD matrix inversion
    Significant perf boost.

--- ViroRenderer/VROMath.cpp
+++ ViroRenderer/VROMath.cpp
@@ -511,67 +511,136 @@ void VROMathTransposeMatrix(const float *src, float *transpose) {
     transpose[15] = src[15];
 }
 
+static __attribute__((__always_inline__, __nodebug__)) void invert4x4(const float *src, float *dst) {
+    typedef __attribute__((__ext_vector_type__(4))) float float4;
+    
+    float4 row0, row1, row2, row3;
+    float4 col0, col1, col2, col3;
+    float4 det, tmp1;
+    
+    /* Load matrix: */
+    
+    col0 = ((float4 *) src)[0];
+    col1 = ((float4 *) src)[1];
+    col2 = ((float4 *) src)[2];
+    col3 = ((float4 *) src)[3];
+    
+    /* Transpose: */
+    
+    tmp1 = __builtin_shufflevector(col0, col2, 0, 4, 1, 5);
+    row1 = __builtin_shufflevector(col1, col3, 0, 4, 1, 5);
+    
+    row0 = __builtin_shufflevector(tmp1, row1, 0, 4, 1, 5);
+    row1 = __builtin_shufflevector(tmp1, row1, 2, 6, 3, 7);
+    
+    tmp1 = __builtin_shufflevector(col0, col2, 2, 6, 3, 7);
+    row3 = __builtin_shufflevector(col1, col3, 2, 6, 3, 7);
+    
+    row2 = __builtin_shufflevector(tmp1, row3, 0, 4, 1, 5);
+    row3 = __builtin_shufflevector(tmp1, row3, 2, 6, 3, 7);
+    
+    /* Compute adjoint: */
+    
+    row1 = __builtin_shufflevector(row1, row1, 2, 3, 0, 1);
+    row3 = __builtin_shufflevector(row3, row3, 2, 3, 0, 1);
+    
+    tmp1 = row2 * row3;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    
+    col0 = row1 * tmp1;
+    col1 = row0 * tmp1;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col0 = row1 * tmp1 - col0;
+    col1 = row0 * tmp1 - col1;
+    col1 = __builtin_shufflevector(col1, col1, 2, 3, 4, 5);
+    
+    tmp1 = row1 * row2;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    
+    col0 = row3 * tmp1 + col0;
+    col3 = row0 * tmp1;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col0 = col0 - row3 * tmp1;
+    col3 = row0 * tmp1 - col3;
+    col3 = __builtin_shufflevector(col3, col3, 2, 3, 4, 5);
+    
+    tmp1 = __builtin_shufflevector(row1, row1, 2, 3, 4, 5) * row3;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    row2 = __builtin_shufflevector(row2, row2, 2, 3, 4, 5);
+    
+    col0 = row2 * tmp1 + col0;
+    col2 = row0 * tmp1;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col0 = col0 - row2 * tmp1;
+    col2 = row0 * tmp1 - col2;
+    col2 = __builtin_shufflevector(col2, col2, 2, 3, 4, 5);
+    
+    tmp1 = row0 * row1;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    
+    col2 = row3 * tmp1 + col2;
+    col3 = row2 * tmp1 - col3;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col2 = row3 * tmp1 - col2;
+    col3 = col3 - row2 * tmp1;
+    
+    tmp1 = row0 * row3;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    
+    col1 = col1 - row2 * tmp1;
+    col2 = row1 * tmp1 + col2;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col1 = row2 * tmp1 + col1;
+    col2 = col2 - row1 * tmp1;
+    
+    tmp1 = row0 * row2;
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 1, 0, 7, 6);
+    
+    col1 = row3 * tmp1 + col1;
+    col3 = col3 - row1 * tmp1;
+    
+    tmp1 = __builtin_shufflevector(tmp1, tmp1, 2, 3, 4, 5);
+    
+    col1 = col1 - row3 * tmp1;
+    col3 = row1 * tmp1 + col3;
+    
+    /* Compute determinant: */
+    
+    det = row0 * col0;
+    det = __builtin_shufflevector(det, det, 2, 3, 4, 5) + det;
+    det = __builtin_shufflevector(det, det, 1, 0, 7, 6) + det;
+    
+    /* Compute reciprocal of determinant: */
+    
+    det = 1.0f / det;
+    
+    /* Multiply matrix of cofactors with reciprocal of determinant: */
+    
+    col0 = col0 * det;
+    col1 = col1 * det;
+    col2 = col2 * det;
+    col3 = col3 * det;
+    
+    /* Store inverted matrix: */
+    
+    ((float4 *) dst)[0] = col0;
+    ((float4 *) dst)[1] = col1;
+    ((float4 *) dst)[2] = col2;
+    ((float4 *) dst)[3] = col3;
+}
+
 bool VROMathInvertMatrix(const float *src, float *inverse) {
-    float temp[16];
-    int i, j, k, swap;
-    float t;
-    
-    for (int i = 0; i < 16; i++) {
-        temp[i] = src[i];
-    }
-    
-    VROMathMakeIdentity(inverse);
-    
-    for (i = 0; i < 4; i++) {
-        /*
-         * Look for largest element in column
-         */
-        swap = i;
-        for (j = i + 1; j < 4; j++) {
-            if (std::abs(temp[(j << 2) + i]) > std::abs(temp[(i << 2) + i])) {
-                swap = j;
-            }
-        }
-        
-        if (swap != i) {
-            /*
-             * Swap rows.
-             */
-            for (k = 0; k < 4; k++) {
-                t = temp[(i << 2) + k];
-                temp[(i << 2) + k] = temp[(swap << 2) + k];
-                temp[(swap << 2) + k] = t;
-                
-                t = inverse[(i << 2) + k];
-                inverse[(i << 2) + k] = inverse[(swap << 2) + k];
-                
-                inverse[(swap << 2) + k] = t;
-            }
-        }
-        
-        if (temp[(i << 2) + i] == 0) {
-            /*
-             * No non-zero pivot. The matrix is singular, which shouldn't
-             * happen. This means the user gave us a bad matrix.
-             */
-            return false;
-        }
-        
-        t = temp[(i << 2) + i];
-        for (k = 0; k < 4; k++) {
-            temp[(i << 2) + k] /= t;
-            inverse[(i << 2) + k] = inverse[(i << 2) + k] / t;
-        }
-        for (j = 0; j < 4; j++) {
-            if (j != i) {
-                t = temp[(j << 2) + i];
-                for (k = 0; k < 4; k++) {
-                    temp[(j << 2) + k] -= temp[(i << 2) + k] * t;
-                    inverse[(j << 2) + k] = inverse[(j << 2) + k] - inverse[(i << 2) + k] * t;
-                }
-            }
-        }
-    }
+    invert4x4(src, inverse);
     return true;
 }
 
