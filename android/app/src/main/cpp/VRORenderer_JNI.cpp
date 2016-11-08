972c51a48d2fba233644d0c76e863399b1ab1986
Raj Advani <radvani@viromedia.com>
2016-11-08 12:45:54 -0500

    [android]: rename treasure hunt classes to VRO

--- android/app/src/main/cpp/VRORenderer_JNI.cpp
+++ android/app/src/main/cpp/VRORenderer_JNI.cpp
@@ -1,17 +1,10 @@
-/* Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+//  VRORenderer_JNI.cpp
+//  ViroRenderer
+//
+//  Created by Raj Advani on 11/8/16.
+//  Copyright © 2016 Viro Media. All rights reserved.
+//
 
 #include <android/log.h>
 #include <jni.h>
@@ -20,7 +13,7 @@
 
 #include "vr/gvr/capi/include/gvr.h"
 #include "vr/gvr/capi/include/gvr_audio.h"
-#include "treasure_hunt_renderer.h"
+#include "VROSceneRendererCardboard.h"
 
 #define JNI_METHOD(return_type, method_name) \
   JNIEXPORT return_type JNICALL              \
--- android/app/src/main/cpp/VROSceneRendererCardboard.h
+++ android/app/src/main/cpp/VROSceneRendererCardboard.h
@@ -1,17 +1,10 @@
-/* Copyright 2016 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+//  VROSceneRendererCardboard.h
+//  ViroRenderer
+//
+//  Created by Raj Advani on 11/8/16.
+//  Copyright © 2016 Viro Media. All rights reserved.
+//
 
 #ifndef TREASUREHUNT_APP_SRC_MAIN_JNI_TREASUREHUNTRENDERER_H_  // NOLINT
 #define TREASUREHUNT_APP_SRC_MAIN_JNI_TREASUREHUNTRENDERER_H_  // NOLINT
