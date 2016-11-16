728fd7fd51fbe6ab857fa3fecb3ea5a070b76ae4
Raj Advani <radvani@viromedia.com>
2016-11-16 14:38:28 -0800

    [android]: video texture documentation
    Explanatory comments for the more opaque
    data structures.

--- android/app/src/main/cpp/VROVideoTextureAndroid.h
+++ android/app/src/main/cpp/VROVideoTextureAndroid.h
@@ -26,6 +26,11 @@ enum class VROCodecType {
     Audio
 };
 
+/*
+ Wraps an output buffer from a codec. Output buffers contain
+ decoded data, ready to be rendered, and an associated
+ presentation time that tells us when we should render the data.
+ */
 class VROCodecOutputBuffer {
 public:
     ssize_t index;
@@ -37,6 +42,30 @@ public:
     }
 };
 
+/*
+ Wrapper over codecs. Codecs are responsible for decoding media
+ data into their raw form where they can be displayed as textures
+ or played as audio. Codecs contain input buffers, where encoded
+ media is written, and output buffers, where decoded media is
+ read. The typical cycle for a single blob of data is:
+
+ 1. Read the encoded data from media
+ 2. Dequeue an input buffer: dequeueInputBuffer
+ 3. Write the encoded data to said buffer: getInputBuffer
+
+ 4. Wait... (while the codec decodes)
+    when finished, the decoded data is placed in an output buffer
+
+ 5. Dequeue the output buffer: dequeueOutputBuffer
+ 6. Read the data from the output buffer: getOutputBuffer
+ 7. Add the output buffer to the ready to display queue: pushOutputBuffer
+
+ 8. Wait... (until the buffer's presentation time)
+ 9. Get the ready to display buffer: nextOutputBuffer
+ 10. Render the buffer using an audio or video sink
+ 11. Pop the buffer off the ready to display queue: popOutputBuffer
+ 12. Release the buffer back to the codec: releaseOutputBuffer
+ */
 class VROCodec {
 public:
 
@@ -122,6 +151,11 @@ private:
 
 };
 
+/*
+ Data structure used for communication between the VROVideoTextureAndroid
+ and the VROVideoLooper. After the looper has started, this should only be
+ modified on the looper thread.
+ */
 class VROMediaData {
 
 public:
@@ -131,6 +165,9 @@ public:
     int32_t deviceAudioBufferSize;
     int32_t sourceAudioSampleRate;
     int32_t audioNumChannels;
+
+    bool muted;
+    float volume;
 
     ANativeWindow *window;
     AMediaExtractor *extractor;
@@ -162,13 +199,31 @@ public:
     }
 };
 
+/*
+ More data structures used for communication with VROVideoLooper.
+ */
 typedef struct {
     VROMediaData *data;
     int64_t seekTime;
 } VROVideoSeek;
 
+typedef struct {
+    VROMediaData *data;
+    float volume;
+} VROVideoVolume;
+
+typedef struct {
+    VROMediaData *data;
+    bool muted;
+} VROVideoMute;
+
 class VROVideoLooper;
 
+/*
+ Renders video to a texture and plays the associated audio.
+ The VROVideoLooper is the underlying workhorse class for
+ rendering video.
+ */
 class VROVideoTextureAndroid : public VROVideoTexture {
 
 public:
@@ -205,34 +260,65 @@ private:
 };
 
 /*
- * Runs on another thread decoding the video. Post messages
- * to this thread using the VROLooper interface.
+ Runs on a separate thread decoding and rendering the video.
+ The VROVideoTexture communicates with this object via the
+ VROLooper interface.
  */
 class VROVideoLooper : public VROLooper {
 
 public:
 
-    //VROVideoLooper();
     virtual ~VROVideoLooper();
-
     void handle(int what, void* obj);
 
 private:
 
-    VROPCMAudioPlayer *_audio = nullptr;
-
+    /*
+     Top level function that extracts data from the media tracks,
+     decodes them via their associated codecs, and writes the data
+     into the corresponding video or audio sinks.
+     */
     void doCodecWork(VROMediaData *d);
 
+    /*
+     Write the latest data from the given media extractor into the
+     provided codec, for decoding.
+     */
     void writeToCodec(VROCodec *codec, AMediaExtractor *extractor, bool *outSawInputEOS);
+
+    /*
+     Read the latest decoded data from the given codec, placing the
+     results in one of the codec's output buffers. From there the data
+     can be written to an audio or video sink.
+     */
     void readFromCodec(VROCodec *codec, bool *outSawOutputEOS);
 
+    /*
+     Render the first available output buffer in either of the given
+     codecs, delaying until the first available buffer's presentationTime
+     if necessary.
+     */
     void writeToSinks(VROCodec *videoCodec, VROCodec *audioCodec, int64_t *renderStart);
     void writeToVideoSink(VROCodec *codec, int64_t *renderStart);
     void writeToAudioSink(VROCodec *codec, int64_t *renderStart);
 
+    /*
+     Compute the amount of time we need to delay before rendering
+     the given buffer. If renderStart (which tells us when the video
+     started playing) is not initialized, it may be initialized here.
+     */
     int64_t computeDelayToRender(VROCodecOutputBuffer buffer, int64_t *renderStart);
+
+    /*
+     Render the given buffer to audio or video, immediately.
+     */
     void renderVideo(VROCodecOutputBuffer buffer, VROCodec *codec);
     void renderAudio(VROCodecOutputBuffer buffer, VROCodec *codec);
+
+    /*
+     The audio player for playing audio tracks via raw PCM data.
+     */
+    VROPCMAudioPlayer *_audio = nullptr;
 
 };
 
