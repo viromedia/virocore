e279ae758dce85b0e0bd5609dd21e69321845615
Raj Advani <radvani@viromedia.com>
2017-04-03 18:08:59 -0400

    VIRO-40: Android normal maps
    Android support for all recent shader
    changes.

--- android/app/src/main/assets/constant_fsh.glsl
+++ android/app/src/main/assets/constant_fsh.glsl
@@ -1,17 +1,14 @@
 #version 300 es
-#include phong_functions_fsh
+#include constant_functions_fsh
 
-uniform highp vec3 camera_position;
 uniform lowp vec4 material_diffuse_surface_color;
 uniform lowp float material_diffuse_intensity;
 uniform lowp float material_alpha;
-uniform lowp float material_shininess;
-
-uniform sampler2D specular_texture;
 
 #pragma surface_modifier_uniforms
+#pragma fragment_modifier_uniforms
 
-in lowp vec3 v_normal;
+in lowp mat3 v_tbn;
 in highp vec2 v_texcoord;
 in highp vec3 v_surface_position;
 
@@ -21,14 +18,15 @@ void main() {
     _surface.diffuse_color = material_diffuse_surface_color;
     _surface.diffuse_texcoord = v_texcoord;
     _surface.diffuse_intensity = material_diffuse_intensity;
-    _surface.shininess = material_shininess;
-    _surface.specular_texcoord = v_texcoord;
     _surface.alpha = material_alpha;
-    _surface.normal = v_normal;
+    _surface.normal = v_tbn[2];
     _surface.position = v_surface_position;
 
 #pragma surface_modifier_body
 
-    frag_color = phong_lighting_diffuse_fixed(_surface, camera_position, specular_texture);
+    lowp vec4 _output_color = vec4(_surface.diffuse_color.xyz, _surface.alpha * _surface.diffuse_color.a);
+    
+#pragma fragment_modifier_body
+    
+    frag_color = _output_color;
 }
-
