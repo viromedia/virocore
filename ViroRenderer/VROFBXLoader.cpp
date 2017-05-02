eb6d1522618c9473ee2e670ac357f2fe31e3f5de
Raj Advani <radvani@viromedia.com>
2017-05-01 23:14:32 -0700

    Add protobuf dependency
    + Universal static library for protobufs
    + Nodes.pb definition
    + Stub for FBX Loader

---
+++ ViroRenderer/VROFBXLoader.cpp
@@ -0,0 +1,9 @@
+//
+//  VROFBXLoader.cpp
+//  ViroRenderer
+//
+//  Created by Raj Advani on 5/1/17.
+//  Copyright © 2017 Viro Media. All rights reserved.
+//
+
+#include "VROFBXLoader.h"
---
+++ ViroRenderer/VROFBXLoader.h
@@ -0,0 +1,14 @@
+//
+//  VROFBXLoader.hpp
+//  ViroRenderer
+//
+//  Created by Raj Advani on 5/1/17.
+//  Copyright © 2017 Viro Media. All rights reserved.
+//
+
+#ifndef VROFBXLoader_h
+#define VROFBXLoader_h
+
+#include <stdio.h>
+
+#endif /* VROFBXLoader_h */
