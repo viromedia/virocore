7b23dfe7da15db24bd5bc89f4d7809f4fb5b0e09
Raj Advani <radvani@viromedia.com>
2017-06-07 23:30:02 -0400

    VIRO-1338 [AR]: VROSurface texture transforms
    ARKit uses texture-coordinate transforms to fit the
    camera’s background texture to the viewport. Add
    support for texture transforms in VROSurface.

--- ViroRenderer/VROSurface.cpp
+++ ViroRenderer/VROSurface.cpp
@@ -12,6 +12,7 @@
 #include "VROGeometryElement.h"
 #include "VROMaterial.h"
 #include "VROAnimationFloat.h"
+#include "VROLog.h"
 #include "stdlib.h"
 
 std::shared_ptr<VROSurface> VROSurface::createSurface(float width, float height,
@@ -83,6 +84,39 @@ void VROSurface::setY(float y) {
     }, _y, y));
 }
 
+void VROSurface::setU0(float u0) {
+    animate(std::make_shared<VROAnimationFloat>([](VROAnimatable *const animatable, float v) {
+        ((VROSurface *)animatable)->_u0 = v;
+        ((VROSurface *)animatable)->updateSurface();
+    }, _u0, u0));
+}
+
+void VROSurface::setU1(float u1) {
+    animate(std::make_shared<VROAnimationFloat>([](VROAnimatable *const animatable, float v) {
+        ((VROSurface *)animatable)->_u1 = v;
+        ((VROSurface *)animatable)->updateSurface();
+    }, _u1, u1));
+}
+
+void VROSurface::setV0(float v0) {
+    animate(std::make_shared<VROAnimationFloat>([](VROAnimatable *const animatable, float v) {
+        ((VROSurface *)animatable)->_v0 = v;
+        ((VROSurface *)animatable)->updateSurface();
+    }, _v0, v0));
+}
+
+void VROSurface::setV1(float v1) {
+    animate(std::make_shared<VROAnimationFloat>([](VROAnimatable *const animatable, float v) {
+        ((VROSurface *)animatable)->_v1 = v;
+        ((VROSurface *)animatable)->updateSurface();
+    }, _v1, v1));
+}
+
+void VROSurface::setTexcoordTransform(VROMatrix4f transform) {
+    _texcoordTransform = transform;
+    updateSurface();
+}
+
 void VROSurface::buildGeometry(float x, float y, float width, float height,
                                float u0, float v0, float u1, float v1,
                                std::vector<std::shared_ptr<VROGeometrySource>> &sources,
@@ -118,38 +152,51 @@ void VROSurface::buildSurface(VROShapeVertexLayout *vertexLayout,
                               float u0, float v0, float u1, float v1) {
     float z = 0;
     
+    VROVector3f BL = { u0, v1, 0 };
+    BL = _texcoordTransform.multiply(BL);
+    
     vertexLayout[0].x = left;
     vertexLayout[0].y = bottom;
     vertexLayout[0].z = z;
-    vertexLayout[0].u = u0;
-    vertexLayout[0].v = v1;
+    vertexLayout[0].u = BL.x;
+    vertexLayout[0].v = BL.y;
     vertexLayout[0].nx = 0;
     vertexLayout[0].ny = 0;
     vertexLayout[0].nz = 1;
     
+    VROVector3f BR = { u1, v1, 0 };
+    BR = _texcoordTransform.multiply(BR);
+    
+    
     vertexLayout[1].x = right;
     vertexLayout[1].y = bottom;
     vertexLayout[1].z = z;
-    vertexLayout[1].u = u1;
-    vertexLayout[1].v = v1;
+    vertexLayout[1].u = BR.x;
+    vertexLayout[1].v = BR.y;
     vertexLayout[1].nx = 0;
     vertexLayout[1].ny = 0;
     vertexLayout[1].nz = 1;
     
+    VROVector3f TR = { u1, v0, 0 };
+    TR = _texcoordTransform.multiply(TR);
+    
     vertexLayout[2].x = right;
     vertexLayout[2].y = top;
     vertexLayout[2].z = z;
-    vertexLayout[2].u = u1;
-    vertexLayout[2].v = v0;
+    vertexLayout[2].u = TR.x;
+    vertexLayout[2].v = TR.y;
     vertexLayout[2].nx = 0;
     vertexLayout[2].ny = 0;
     vertexLayout[2].nz = 1;
     
+    VROVector3f TL = { u0, v0, 0 };
+    TL = _texcoordTransform.multiply(TL);
+    
     vertexLayout[3].x = left;
     vertexLayout[3].y = top;
     vertexLayout[3].z = z;
-    vertexLayout[3].u = u0;
-    vertexLayout[3].v = v0;
+    vertexLayout[3].u = TL.x;
+    vertexLayout[3].v = TL.y;
     vertexLayout[3].nx = 0;
     vertexLayout[3].ny = 0;
     vertexLayout[3].nz = 1;
