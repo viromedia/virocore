bc451f446b4a84f3ed1fb5fbe24a0f94336971a2
Manish Bodhankar <manish@viromedia.com>
2017-10-27 17:04:37 -0700

    Adding yes / no button

--- android/renderertest/src/androidTest/java/com/viromedia/renderertest/tests/ViroBaseTest.java
+++ android/renderertest/src/androidTest/java/com/viromedia/renderertest/tests/ViroBaseTest.java
@@ -9,18 +9,20 @@
 
 package com.viromedia.renderertest.tests;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.test.rule.ActivityTestRule;
 import android.util.Log;
 
 import com.viro.renderer.ARHitTestResult;
 import com.viro.renderer.jni.EventDelegate;
-import com.viro.renderer.jni.Image;
 import com.viro.renderer.jni.Material;
 import com.viro.renderer.jni.Node;
 import com.viro.renderer.jni.Scene;
 import com.viro.renderer.jni.Surface;
 import com.viro.renderer.jni.Texture;
-import com.viro.renderer.jni.TextureFormat;
 import com.viro.renderer.jni.Vector;
 import com.viro.renderer.jni.ViroView;
 import com.viro.renderer.jni.event.ClickState;
@@ -35,6 +37,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.EnumSet;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.Callable;
@@ -88,33 +93,36 @@ public abstract class ViroBaseTest {
 
     private void createBaseTestScene() {
         Node rootNode = mScene.getRootNode();
+        EnumSet<Node.TransformBehavior> transformBehavior = EnumSet.of(Node.TransformBehavior.BILLBOARD);
 
         // Add yes button
         Node yesButton = new Node();
-        Image yesImage = new Image("icon_thumb_up.png", TextureFormat.RGBA8);
-        Texture yesTexture = new Texture(yesImage,
-                TextureFormat.RGBA8, true, true, "LeftRight");
+        Bitmap yesBitmap = getBitmapFromAsset(mActivity, "icon_thumb_up.png");
+        Texture yesTexture = new Texture(yesBitmap,
+                Texture.TextureFormat.RGBA8, true, true);
         Material yesMaterial = new Material();
         Surface yesSurface = new Surface(2, 2, 0, 0, 1, 1);
         yesSurface.setMaterial(yesMaterial);
         yesSurface.setImageTexture(yesTexture);
         yesButton.setGeometry(yesSurface);
-        float[] yesPosition = {-1.5f, -2f, -3.3f};
+        float[] yesPosition = {-1.5f, -3f, -3.3f};
         yesButton.setPosition(new Vector(yesPosition));
+        yesButton.setTransformBehaviors(transformBehavior);
         rootNode.addChildNode(yesButton);
 
         // Add no button
         Node noButton = new Node();
-        Image noImage = new Image("icon_thumb_down.png", TextureFormat.RGBA8);
-        Texture noTexture = new Texture(noImage,
-                TextureFormat.RGBA8, true, true, "LeftRight");
+        Bitmap noBitmap = getBitmapFromAsset(mActivity, "icon_thumb_down.png");
+        Texture noTexture = new Texture(noBitmap,
+                Texture.TextureFormat.RGBA8, true, true);
         Material noMaterial = new Material();
         Surface noSurface = new Surface(2, 2, 0, 0, 1, 1);
         noSurface.setMaterial(noMaterial);
         noSurface.setImageTexture(noTexture);
         noButton.setGeometry(noSurface);
-        float[] noPosition = {1.5f, -2f, -3.3f};
+        float[] noPosition = {1.5f, -3f, -3.3f};
         noButton.setPosition(new Vector(noPosition));
+        noButton.setTransformBehaviors(transformBehavior);
         rootNode.addChildNode(noButton);
         // Add instruction card
     }
@@ -131,6 +139,20 @@ public abstract class ViroBaseTest {
         }
     }
 
+    private Bitmap getBitmapFromAsset(Context context, String filePath) {
+        AssetManager assetManager = context.getAssets();
+
+        InputStream istr;
+        Bitmap bitmap = null;
+        try {
+            istr = assetManager.open(filePath);
+            bitmap = BitmapFactory.decodeStream(istr);
+        } catch (IOException e) {
+            System.err.println("Error loading image from assets");
+        }
+
+        return bitmap;
+    }
     protected EventDelegate getGenericDelegate(String delegateTag) {
         EventDelegate delegateJni = new EventDelegate();
         delegateJni.setEventEnabled(EventDelegate.EventAction.ON_HOVER, false);
--- android/renderertest/src/androidTest/java/com/viromedia/renderertest/tests/ViroDirectionalLightTest.java
+++ android/renderertest/src/androidTest/java/com/viromedia/renderertest/tests/ViroDirectionalLightTest.java
@@ -10,29 +10,11 @@
 package com.viromedia.renderertest.tests;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.viro.renderer.jni.AmbientLight;
-import com.viro.renderer.jni.Box;
-import com.viro.renderer.jni.DirectionalLight;
-import com.viro.renderer.jni.Image;
-import com.viro.renderer.jni.Material;
-import com.viro.renderer.jni.Node;
-import com.viro.renderer.jni.OmniLight;
-import com.viro.renderer.jni.Spotlight;
-import com.viro.renderer.jni.Text;
-import com.viro.renderer.jni.Texture;
-import com.viro.renderer.jni.TextureFormat;
-import com.viro.renderer.jni.Vector;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -46,78 +28,12 @@ public class ViroDirectionalLightTest extends ViroBaseTest{
 
     @Override
     void configureTestScene() {
-        Node rootNode = mScene.getRootNode();
-        
-        testSceneLighting(rootNode);
     }
 
     @Override
     void callbackEverySecond() {
 
     }
-
-    private List<Node> testBox() {
-        Node node1 = new Node();
-        Node node2 = new Node();
-
-        Node node3 = new Node();
-        Text textJni = new Text(mViroView.getViroContext(), "Test text 1 2 3", "Roboto", 24,
-                Color.WHITE, 10, 4, Text.HorizontalAlignment.CENTER, Text.VerticalAlignment.CENTER, Text.LineBreakMode.NONE,
-                Text.ClipMode.CLIP_TO_BOUNDS, 1);
-
-        float[] position = {0, -1, -2};
-        node3.setPosition(new Vector(position));
-        node3.setGeometry(textJni);
-        //node3.setEventDelegate(getGenericDelegate("Text"));
-
-        // Create a new material with a diffuseTexture set to the image "boba.png"
-        Image bobaImage = new Image("boba.png", TextureFormat.RGBA8);
-
-        Texture bobaTexture = new Texture(bobaImage, TextureFormat.RGBA8, true, true);
-        Material material = new Material();
-//        material.setTexture(bobaTexture, "diffuseTexture");
-        material.setColor(Color.BLUE, "diffuseColor");
-        material.setLightingModel("Blinn");
-
-        // Creation of ViroBox to the right and billboarded
-        Box boxGeometry = new Box(2,4,2);
-        node1.setGeometry(boxGeometry);
-        float[] boxPosition = {5,0,-3};
-        node1.setPosition(new Vector(boxPosition));
-        boxGeometry.setMaterials(Arrays.asList(material));
-        EnumSet<Node.TransformBehavior> behaviors = EnumSet.of(Node.TransformBehavior.BILLBOARD);
-        node1.setTransformBehaviors(behaviors);
-        node1.setEventDelegate(getGenericDelegate("Box"));
-
-        Box boxGeometry2 = new Box(2, 2, 2);
-        node2.setGeometry(boxGeometry2);
-        float[] boxPosition2 = {-2, 0, -3};
-        node2.setPosition(new Vector(boxPosition2));
-        boxGeometry2.setMaterials(Arrays.asList(material));
-        node2.setEventDelegate(getGenericDelegate("Box2"));
-
-        return Arrays.asList(node1, node2, node3);
-    }
-
-    private void testSceneLighting(Node node) {
-        float[] lightDirection = {0, 0, -1};
-        AmbientLight ambientLightJni = new AmbientLight(Color.BLACK, 1000.0f);
-        node.addLight(ambientLightJni);
-
-        DirectionalLight directionalLightJni = new DirectionalLight(Color.BLUE, 1000.0f, new Vector(lightDirection));
-        node.addLight(directionalLightJni);
-
-        float[] omniLightPosition = {1,0,0};
-        OmniLight omniLightJni = new OmniLight(Color.RED, 1000.0f, 1, 10, new Vector(omniLightPosition));
-        node.addLight(omniLightJni);
-
-        float[] spotLightPosition = {-2, 0, 3};
-        Spotlight spotLightJni = new Spotlight(Color.YELLOW, 1000.0f, 1, 10, new Vector(spotLightPosition),
-                new Vector(lightDirection), 2, 10);
-        node.addLight(spotLightJni);
-    }
-
-
 
     @Test
     public void testDirectionalLight_direction() {
