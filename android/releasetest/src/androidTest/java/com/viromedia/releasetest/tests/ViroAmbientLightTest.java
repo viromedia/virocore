cce87a234f4c0ca95c2695d816d8c8789af8452a
Manish Bodhankar <manish@viromedia.com>
2017-11-02 10:23:00 -0700

    AmbientLight tests

--- android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroAmbientLightTest.java
+++ android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroAmbientLightTest.java
@@ -9,15 +9,24 @@
 
 package com.viromedia.releasetest.tests;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.graphics.Color;
+import android.support.test.espresso.core.deps.guava.collect.Iterables;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
+
+import com.viro.renderer.jni.AmbientLight;
+import com.viro.renderer.jni.DirectionalLight;
+import com.viro.renderer.jni.Material;
+import com.viro.renderer.jni.Node;
+import com.viro.renderer.jni.Sphere;
+import com.viro.renderer.jni.Surface;
+import com.viro.renderer.jni.Vector;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by manish on 10/25/17.
@@ -26,18 +35,106 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ViroAmbientLightTest extends ViroBaseTest {
     private static final String TAG = ViroAmbientLightTest.class.getName();
-
+    private static final int INFLUENCE_BITMASK = 1;
+    private AmbientLight mAmbientLight;
+    private Node mVSurface;
+    private Node mHSurface;
+    private Node mSphere;
     @Override
     void configureTestScene() {
+        mAmbientLight = new AmbientLight();
+        mAmbientLight.setInfluenceBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addLight(mAmbientLight);
+
+        final DirectionalLight directionalLight = new DirectionalLight();
+        directionalLight.setDirection(new Vector(-1, 0, 0));
+        directionalLight.setInfluenceBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addLight(directionalLight);
+        // Configure verticle surface
+        mVSurface = new Node();
+        final Surface surface = new Surface(5, 5);
+        final float[] surfacePos = {0, 0, -5};
+        mVSurface.setPosition(new Vector(surfacePos));
+        final Material surfaceMaterial = new Material();
+        surfaceMaterial.setDiffuseColor(Color.WHITE);
+        surfaceMaterial.setLightingModel(Material.LightingModel.BLINN);
+        surface.setMaterials(Arrays.asList(surfaceMaterial));
+        mVSurface.setGeometry(surface);
+        mVSurface.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addChildNode(mVSurface);
+
+        // Configure horizontal surface
+        mHSurface = new Node();
+        final Surface hSurface = new Surface(5, 5);
+        final float[] hSurfacePos = {0, -2f, -5};
+        mHSurface.setPosition(new Vector(hSurfacePos));
+        final Material hSurfaceMaterial = new Material();
+        hSurfaceMaterial.setDiffuseColor(Color.WHITE);
+        hSurfaceMaterial.setLightingModel(Material.LightingModel.BLINN);
+        hSurface.setMaterials(Arrays.asList(hSurfaceMaterial));
+        mHSurface.setGeometry(hSurface);
+        mHSurface.setRotation(new Vector(-90, 0, 0));
+        mHSurface.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addChildNode(mHSurface);
+
+        // Configure sphere
+        mSphere = new Node();
+        final Sphere sphere = new Sphere(0.5f);
+        final Material sphereMaterial = new Material();
+        sphereMaterial.setDiffuseColor(Color.WHITE);
+        sphereMaterial.setLightingModel(Material.LightingModel.BLINN);
+        mSphere.setShadowCastingBitMask(INFLUENCE_BITMASK);
+        mSphere.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        mSphere.setGeometry(sphere);
+        final float[] spherePos = {0, -0.5f, -2f};
+        mSphere.setPosition(new Vector(spherePos));
+        sphere.setMaterials(Arrays.asList(sphereMaterial));
+        mScene.getRootNode().addChildNode(mSphere);
     }
 
     @Test
-    public void dummy_test() {
+    public void testDirectionalLight() {
         // Context of the app under test.
-        Log.d(TAG, "useApp called");
-        final Context appContext = InstrumentationRegistry.getTargetContext();
+        testSetColor();
+        testSetIntensity();
+        testSetInfluenceBitMask();
+    }
 
-        assertEquals("com.viromedia.renderertest.gvr", appContext.getPackageName());
-        assertEquals(true, mActivity.isGlInitialized());
+    private void testSetColor() {
+        final List<Integer> colors = Arrays.asList(Color.WHITE, Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN);
+
+        final Iterator<Integer> itr = Iterables.cycle(colors).iterator();
+
+        mMutableTestMethod = () -> {
+            mAmbientLight.setColor(itr.next());
+        };
+        assertPass("Cycling colors through WHITE, RED, GREEN, BLUE, MAGENTA, CYAN", () -> {
+            mAmbientLight.setColor(Color.GREEN);
+        });
+    }
+
+    private void testSetIntensity() {
+        mMutableTestMethod = () -> {
+            if (mAmbientLight != null) {
+                final float currentIntensity = mAmbientLight.getIntensity();
+                mAmbientLight.setIntensity((currentIntensity + 200) % 2000);
+            }
+        };
+        assertPass("Cycling intensity from 0 to 2000, with +200 every second", () -> {
+            mAmbientLight.setIntensity(1000);
+        });
+    }
+
+    private void testSetInfluenceBitMask() {
+        mMutableTestMethod = () -> {
+            if (mAmbientLight != null) {
+                final int currentBitMask = mAmbientLight.getInfluenceBitMask();
+                mAmbientLight.setInfluenceBitMask(currentBitMask == INFLUENCE_BITMASK ? 2 : INFLUENCE_BITMASK);
+            }
+        };
+
+        assertPass("Alternating ambientLight's influence betweeen 1 and 2", () -> {
+            mSphere.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        });
     }
 }
