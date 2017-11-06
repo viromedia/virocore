c59a8b4d7ad0d814a04e655f05a241aa7a984061
Vik Advani <vadvani@viromedia.com>
2017-11-06 13:04:48 -0800

    Add events release test.

---
+++ android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroEventsTest.java
@@ -0,0 +1,255 @@
+package com.viromedia.releasetest.tests;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import com.viro.renderer.jni.Box;
+import com.viro.renderer.jni.DirectionalLight;
+import com.viro.renderer.jni.Material;
+import com.viro.renderer.jni.Node;
+import com.viro.renderer.jni.Object3D;
+import com.viro.renderer.jni.Sphere;
+import com.viro.renderer.jni.Text;
+import com.viro.renderer.jni.Vector;
+import com.viro.renderer.jni.event.ClickListener;
+import com.viro.renderer.jni.event.ClickState;
+import com.viro.renderer.jni.event.FuseListener;
+import com.viro.renderer.jni.event.GesturePinchListener;
+import com.viro.renderer.jni.event.HoverListener;
+import com.viro.renderer.jni.event.PinchState;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by vadvani on 11/3/17.
+ */
+
+public class ViroEventsTest extends ViroBaseTest {
+    ClickListener boxClickListener;
+    private Node boxNode;
+    private Object3D objectNode;
+    private Node sphereNode;
+    private Text eventText;
+
+    @Override
+    void configureTestScene() {
+        DirectionalLight light = new DirectionalLight(Color.WHITE, 1000.0f, new Vector(0, 0, -1f));
+        mScene.getRootNode().addLight(light);
+
+        //add box node.
+        Box box = new Box(1, 1, 1);
+        Material boxOneMaterial = new Material();
+        boxOneMaterial.setDiffuseColor(Color.RED);
+        boxOneMaterial.setLightingModel(Material.LightingModel.BLINN);
+        box.setMaterials(Arrays.asList(boxOneMaterial));
+        boxNode = new Node();
+        boxNode.setPosition(new Vector(-1.5f, 1f, -3f));
+        boxNode.setGeometry(box);
+
+
+        //add object node.
+        objectNode = new Object3D();
+        objectNode.loadModel(Uri.parse("file:///android_asset/object_star_anim.vrx"), Object3D.Type.FBX, null);
+        objectNode.setPosition(new Vector(0.0f, 1f, -3f));
+
+        //add sphere node.
+        Sphere sphere = new Sphere(.5f);
+        Material sphereMaterial = new Material();
+        sphereMaterial.setDiffuseColor(Color.YELLOW);
+        sphereMaterial.setLightingModel(Material.LightingModel.BLINN);
+        sphere.setMaterials(Arrays.asList(boxOneMaterial));
+        sphereNode = new Node();
+        sphereNode.setGeometry(sphere);
+        sphereNode.setPosition(new Vector(1.5f, 1.0f, -3f));
+
+
+        mScene.getRootNode().addChildNode(boxNode);
+        mScene.getRootNode().addChildNode(objectNode);
+        mScene.getRootNode().addChildNode(sphereNode);
+        eventText = new Text(mViroView.getViroContext(),
+                "Event not registered.", "Roboto", 20, Color.WHITE, 1f, 1f, Text.HorizontalAlignment.LEFT,
+                Text.VerticalAlignment.TOP, Text.LineBreakMode.WORD_WRAP, Text.ClipMode.CLIP_TO_BOUNDS, 0);
+
+        Node textNode = new Node();
+        textNode.setPosition(new Vector(0, 3.0f, -4f));
+        textNode.setGeometry(eventText);
+        mScene.getRootNode().addChildNode(textNode);
+
+    }
+
+    @Test
+    public void testEvents() {
+        testEventsClickListener();
+        testEventsHoverListener();
+        testEventsDragListener();
+        testEventsFuseListener();
+        testEventsTouchpadTouchListener();
+        testEventsTouchpadSwipeListener();
+        testEventsTouchpadScrollListener();
+        //TODO: need to test bubbling of events from child to parent.
+    }
+
+
+    private void testEventsClickListener() {
+        Log.i("ViroEventsTest", "in testEventsClickListener()");
+        //boxNode.setClickListener((source, node, clickState, location) -> {
+        //    eventText.setText("Clicked on box.");
+        //    Log.i("ViroEventsTest", "CLICKED ON BOX!!");
+        //});
+
+        boxClickListener = (source, node, clickState, location) -> {
+            eventText.setText("Clicked on 3d object.");
+            Log.i("ViroEventsTest", "CLICKED ON OBJECT!!");
+        };
+        objectNode.setClickListener(boxClickListener);
+
+        sphereNode.setClickListener((source, node, clickState, location) -> {
+            eventText.setText("Clicked on sphere.");
+        });
+        assertPass("All objects are clickable.");
+    }
+
+    private void testEventsHoverListener() {
+        boxNode.setHoverListener((source, node, isHovering, location) -> {
+            eventText.setText("Hovered over box.");
+        });
+
+        objectNode.setHoverListener((source, node, isHovering, location) -> {
+            eventText.setText("Hovered over 3d object.");
+        });
+
+        sphereNode.setClickListener((source, node, isHovering, location) -> {
+            eventText.setText("Hovered over sphere.");
+        });
+
+        assertPass("All events hover.");
+    }
+
+
+    private void testEventsFuseListener() {
+        boxNode.setFuseListener((source, node) -> {
+            eventText.setText("Set fuse on box.");
+        });
+
+        objectNode.setFuseListener((source, node) -> {
+            eventText.setText("Set fuse on 3d object");
+        });
+
+        sphereNode.setFuseListener((source, node) -> {
+            eventText.setText("Set fuse on Sphere.");
+        });
+
+        assertPass("All nodes respond to onFuse.");
+    }
+
+    private void testEventsDragListener() {
+        boxNode.setDragListener((source, node, worldLocation, localLocation) -> {
+            eventText.setText("Dragging the box. WorldLoc:" + ViroEventsTest.vectorString(worldLocation) + ", LocalLoc:" + ViroEventsTest.vectorString(localLocation));
+        });
+
+        objectNode.setDragListener((source, node, worldLocation, localLocation) -> {
+            eventText.setText("Dragging the object. WorldLoc:" + ViroEventsTest.vectorString(worldLocation) + ", LocalLoc:" + ViroEventsTest.vectorString(localLocation));
+        });
+
+        sphereNode.setDragListener((source, node, worldLocation, localLocation) -> {
+            eventText.setText("Dragging the sphere. WorldLoc:" + ViroEventsTest.vectorString(worldLocation) + ", LocalLoc:" + ViroEventsTest.vectorString(localLocation));
+        });
+
+        assertPass("All objects respond to drag events with world loc. and local loc.", () -> {
+            sphereNode.setDragListener(null);
+            objectNode.setDragListener(null);
+            boxNode.setDragListener(null);
+        });
+    }
+
+    private void testEventsGesturePinchListener() {
+        //(int source, Node node, float scaleFactor, PinchState pinchState)
+        boxNode.setGesturePinchListener((source, node, scaleFactor, pinchState) -> {
+            eventText.setText("Pinching on box.");
+            boxNode.setScale(new Vector(scaleFactor, scaleFactor, scaleFactor));
+        });
+
+        objectNode.setGesturePinchListener((source, node, scaleFactor, pinchState) -> {
+            eventText.setText("Pinching on 3D Object.");
+            objectNode.setScale(new Vector(scaleFactor, scaleFactor, scaleFactor));
+        });
+
+        sphereNode.setGesturePinchListener((source, node, scaleFactor, pinchState) -> {
+            eventText.setText("Pinching on Sphere");
+            sphereNode.setScale(new Vector(scaleFactor, scaleFactor, scaleFactor));
+        });
+        assertPass("All nodes respond to pinch.");
+    }
+
+    private void testEventsGestureRotateListener() {
+
+        boxNode.setGestureRotateListener((source, node, rotateDegrees, rotateState) -> {
+            eventText.setText("Rotating on Box.");
+        });
+
+        objectNode.setGestureRotateListener((source, node, rotateDegrees, rotateState) -> {
+            eventText.setText("Rotating on 3d Object.");
+        });
+
+        sphereNode.setGestureRotateListener((source, node, rotateDegrees, rotateState) -> {
+            eventText.setText("Rotating on Sphere.");
+        });
+
+        assertPass("All nodes respond to rotate.");
+    }
+
+    private void testEventsTouchpadTouchListener() {
+        boxNode.setTouchpadTouchListener((source, node, touchState, x, y) -> {
+            eventText.setText("Touch registered on Box. TouchState: " + touchState.toString());
+        });
+
+        objectNode.setTouchpadTouchListener((source, node, touchState, x, y) -> {
+            eventText.setText("Touch registered on 3d Object. TouchState: " + touchState.toString());
+        });
+
+        sphereNode.setTouchpadTouchListener((source, node, touchState, x, y) -> {
+            eventText.setText("Touch registered on Sphere. TouchState: " + touchState.toString());
+        });
+
+        assertPass("For GearVR: All objects can be touched with touch pad, Touch state changes.");
+    }
+
+    private void testEventsTouchpadSwipeListener() {
+        boxNode.setTouchpadSwipeListener((source, node, swipeState) -> {
+            eventText.setText("Touchpad listener registered on Box. SwipeState: " + swipeState.toString());
+        });
+
+        objectNode.setTouchpadSwipeListener((source, node, swipeState) -> {
+            eventText.setText("Touchpad listener registered on 3d Object. SwipeState: " + swipeState.toString());
+        });
+
+        sphereNode.setTouchpadSwipeListener((source, node, swipeState) -> {
+            eventText.setText("Touchpad listener registered on Sphere. SwipeState: " + swipeState.toString());
+        });
+    }
+
+    private void testEventsTouchpadScrollListener() {
+        boxNode.setTouchpadScrollListener((source, node, x, y) -> {
+            eventText.setText("Scroll listener registered on box. (x,y):" + "[" + x + "," + y + "]");
+        });
+
+        objectNode.setTouchpadScrollListener((source, node, x, y) -> {
+            eventText.setText("Scroll listener registered on object. (x,y):" + "[" + x + "," + y + "]");
+        });
+
+        sphereNode.setTouchpadScrollListener((source, node, x, y) -> {
+            eventText.setText("Scroll listener registered on sphere. (x,y):" + "[" + x + "," + y + "]");
+        });
+    }
+
+    public static String vectorString(Vector vec) {
+        String vectorString = "[" + vec.x + "," + vec.y + "," + vec.x + "]";
+        return vectorString;
+    }
+
+
+}
