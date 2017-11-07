eb99047f9e1946413c411a7a7cd957b636433e8a
Vik Advani <vadvani@viromedia.com>
2017-11-07 00:53:25 -0800

    Added animation transaction release test.

---
+++ android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroAnimationTransactionTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2017-present, ViroMedia, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the  LICENSE file in the
+ * root directory of this source tree. An additional grant  of patent rights can be found in
+ * the PATENTS file in the same directory.
+ */
+
+package com.viromedia.releasetest.tests;
+
+import android.graphics.Color;
+import android.graphics.drawable.AnimatedStateListDrawable;
+
+import com.viro.renderer.jni.AmbientLight;
+import com.viro.renderer.jni.AnimationTimingFunction;
+import com.viro.renderer.jni.AnimationTransaction;
+import com.viro.renderer.jni.Box;
+import com.viro.renderer.jni.Material;
+import com.viro.renderer.jni.Node;
+import com.viro.renderer.jni.Vector;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+/**
+ * Created by vadvani on 11/6/17.
+ */
+
+public class ViroAnimationTransactionTest extends ViroBaseTest {
+
+    private Node boxNode;
+    private Box mBox;
+    @Override
+    void configureTestScene() {
+        AmbientLight light = new AmbientLight(Color.WHITE, 1000.0f);
+        mScene.getRootNode().addLight(light);
+        boxNode = new Node();
+        mBox = new Box(1, 1, 1);
+        Material material = new Material();
+        material.setLightingModel(Material.LightingModel.BLINN);
+        material.setDiffuseColor(Color.BLUE);
+        mBox.setMaterials(Arrays.asList(material));
+        boxNode.setGeometry(mBox);
+        boxNode.setPosition(new Vector(-3,0, -3));
+        mScene.getRootNode().addChildNode(boxNode);
+
+    }
+
+    @Test
+    public void animationTest() {
+            testAnimationPosition();
+            testAnimationRotation();
+            testAnimationScale();
+            testAnimationOpacity();
+            testAnimationEasing();
+            testAnimationMaterial();
+            testAnimationChained();
+    }
+
+    private void testAnimationPosition() {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(3000);
+        AnimationTransaction.setTimingFunction(AnimationTimingFunction.EaseOut);
+            boxNode.setPosition(new Vector(3, 0, -3));
+        AnimationTransaction.commit();
+        assertPass("Position animates from x -3 to 3.", () -> {
+            boxNode.setPosition(new Vector(0, 0, -3));
+        });
+    }
+
+    private void testAnimationRotation() {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(3000);
+        AnimationTransaction.setTimingFunction(AnimationTimingFunction.EaseOut);
+            boxNode.setRotation(new Vector(0, 45, 45));
+        AnimationTransaction.commit();
+        assertPass("Animate rotation to 45 degrees on y and z axis.", ()->{
+            boxNode.setRotation(new Vector(0, 0, 0));
+        });
+    }
+
+    private void testAnimationScale() {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(3000);
+        AnimationTransaction.setTimingFunction(AnimationTimingFunction.EaseOut);
+            boxNode.setScale(new Vector(.2f, .2f, 2f));
+        AnimationTransaction.commit();
+        assertPass("Animate animates from scale 1 to .2", ()-> {
+            boxNode.setScale(new Vector(1f, 1f, 1f));
+        });
+    }
+
+    private void testAnimationMaterial() {
+        Material material = new Material();
+        material.setLightingModel(Material.LightingModel.BLINN);
+        material.setDiffuseColor(Color.RED);
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(4000);
+            mBox.setMaterials(Arrays.asList(material));
+        AnimationTransaction.commit();
+        assertPass("Material blends from blue to red.");
+    }
+
+    private void testAnimationOpacity() {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(3000);
+        AnimationTransaction.setTimingFunction(AnimationTimingFunction.EaseOut);
+        boxNode.setOpacity(0.2f);
+        AnimationTransaction.commit();
+        assertPass("Animate opacity from 1 to 0.2", ()->{
+            boxNode.setOpacity(1.0f);
+        });
+    }
+
+    private void testAnimationChained() {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(2000);
+        AnimationTransaction.setTimingFunction(AnimationTimingFunction.Linear);
+        boxNode.setPosition(new Vector(0, 0, -6));
+        AnimationTransaction.setFinishCallback((transaction)-> {
+                AnimationTransaction.begin();
+                    AnimationTransaction.setAnimationDuration(2000);
+                    boxNode.setRotation(new Vector(0, 60, 0));
+                AnimationTransaction.commit();
+        });
+        AnimationTransaction.commit();
+
+        assertPass("Chained Animation should move box back then rotate.", ()->{
+            boxNode.setPosition(new Vector(0, 0, -3));
+            boxNode.setRotation(new Vector(0, 0, 0));
+        });
+    }
+
+    private void testAnimationEasing() {
+        for(AnimationTimingFunction animationTimingFunction: AnimationTimingFunction.values()) {
+            testEasing(animationTimingFunction);
+        }
+    }
+
+    private void testEasing(AnimationTimingFunction animTimingFunction) {
+        AnimationTransaction.begin();
+        AnimationTransaction.setAnimationDuration(2000);
+        AnimationTransaction.setTimingFunction(animTimingFunction);
+        boxNode.setPosition(new Vector(0, 0, -5));
+        AnimationTransaction.commit();
+        assertPass("Animation easing is set to:" + animTimingFunction.toString(), ()->{
+            boxNode.setPosition(new Vector(0, 0, -3));
+        });
+    }
+
+}
