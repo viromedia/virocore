cdd242bb76c7db868672a6a18b53dcafeb172f5b
Manish Bodhankar <manish@viromedia.com>
2017-11-10 11:25:44 -0800

    Adding Spotlight tests

--- android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroSpotLightTest.java
+++ android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroSpotLightTest.java
@@ -9,15 +9,24 @@
 
 package com.viromedia.releasetest.tests;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.graphics.Color;
+import android.support.test.espresso.core.deps.guava.collect.Iterables;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
+
+import com.viro.core.AmbientLight;
+import com.viro.core.Material;
+import com.viro.core.Node;
+import com.viro.core.Sphere;
+import com.viro.core.Spotlight;
+import com.viro.core.Surface;
+import com.viro.core.Vector;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by manish on 10/25/17.
@@ -26,19 +35,171 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ViroSpotLightTest extends ViroBaseTest {
     private static final String TAG = ViroSpotLightTest.class.getName();
+    private static final int INFLUENCE_BITMASK = 1;
+    private Spotlight mSpotLight;
 
     @Override
     void configureTestScene() {
+        final AmbientLight ambientLightJni = new AmbientLight(Color.WHITE, 100.0f);
+        mScene.getRootNode().addLight(ambientLightJni);
+
+        // Add directional light in -Z direction
+        mSpotLight = new Spotlight();
+        mSpotLight.setInfluenceBitMask(INFLUENCE_BITMASK);
+
+        mScene.getRootNode().addLight(mSpotLight);
+
+        // Configure verticle surface
+        final Node verticleSurfaceNode = new Node();
+        final Surface surface = new Surface(5, 5);
+        final float[] surfacePos = {0, 0, -5};
+        verticleSurfaceNode.setPosition(new Vector(surfacePos));
+        final Material surfaceMaterial = new Material();
+        surfaceMaterial.setDiffuseColor(Color.WHITE);
+        surfaceMaterial.setLightingModel(Material.LightingModel.BLINN);
+        surface.setMaterials(Arrays.asList(surfaceMaterial));
+        verticleSurfaceNode.setGeometry(surface);
+        verticleSurfaceNode.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addChildNode(verticleSurfaceNode);
+
+        // Configure horizontal surface
+        final Node horizontalSurfaceNode = new Node();
+        final Surface hSurface = new Surface(5, 5);
+        final float[] hSurfacePos = {0, -2f, -5};
+        horizontalSurfaceNode.setPosition(new Vector(hSurfacePos));
+        final Material hSurfaceMaterial = new Material();
+        hSurfaceMaterial.setDiffuseColor(Color.WHITE);
+        hSurfaceMaterial.setLightingModel(Material.LightingModel.BLINN);
+        hSurface.setMaterials(Arrays.asList(hSurfaceMaterial));
+        horizontalSurfaceNode.setGeometry(hSurface);
+        horizontalSurfaceNode.setRotation(new Vector(-90, 0, 0));
+        horizontalSurfaceNode.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        mScene.getRootNode().addChildNode(horizontalSurfaceNode);
+
+        // Configure sphere
+        final Node sphereNode = new Node();
+        final Sphere sphere = new Sphere(0.5f);
+        final Material sphereMaterial = new Material();
+        sphereMaterial.setDiffuseColor(Color.WHITE);
+        sphereMaterial.setLightingModel(Material.LightingModel.BLINN);
+        sphereNode.setShadowCastingBitMask(INFLUENCE_BITMASK);
+        sphereNode.setLightReceivingBitMask(INFLUENCE_BITMASK);
+        sphereNode.setGeometry(sphere);
+        final float[] spherePos = {0, -0.5f, -2f};
+        sphereNode.setPosition(new Vector(spherePos));
+        sphere.setMaterials(Arrays.asList(sphereMaterial));
+        mScene.getRootNode().addChildNode(sphereNode);
 
     }
 
     @Test
-    public void dummy_test() {
-        // Context of the app under test.
-        Log.d(TAG, "useApp called");
-        final Context appContext = InstrumentationRegistry.getTargetContext();
+    public void testSpotlight() {
 
-        assertEquals("com.viromedia.renderertest.gvr", appContext.getPackageName());
-        assertEquals(true, mActivity.isGlInitialized());
+        testSetColor();
+        testSetIntensity();
+        testSetCastsShadow();
+        testSetDirection();
+        testSetPosition();
+        testSetDirection();
+        testSetInnerAngle();
+        testSetOuterAngle();
     }
+
+    /**
+     * Base Light.java props
+     **/
+
+    private void testSetColor() {
+        final List<Integer> colors = Arrays.asList(Color.WHITE, Color.RED, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN);
+
+        final Iterator<Integer> itr = Iterables.cycle(colors).iterator();
+
+        mMutableTestMethod = () -> {
+            mSpotLight.setColor(itr.next());
+        };
+        assertPass("Cycling colors through WHITE, RED, GREEN, BLUE, MAGENTA, CYAN", () -> {
+            mSpotLight.setColor(Color.GREEN);
+        });
+
+    }
+
+    private void testSetIntensity() {
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                final float currentIntensity = mSpotLight.getIntensity();
+                mSpotLight.setIntensity((currentIntensity + 200) % 2000);
+            }
+        };
+        assertPass("Cycling intensity from 0 to 2000, with +200 every second", () -> {
+            mSpotLight.setIntensity(1000);
+        });
+    }
+
+    private void testSetCastsShadow() {
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                mSpotLight.setCastsShadow(!mSpotLight.getCastsShadow());
+            }
+        };
+        assertPass("Toggling casts shadow", () -> {
+            mSpotLight.setCastsShadow(false);
+        });
+    }
+
+    private void testSetInfluenceBitMask() {
+        assertPass("running testColorChange()");
+
+    }
+
+
+    private void testSetDirection() {
+        final List<Vector> directions = Arrays.asList(new Vector(0, 0, -1), new Vector(0, -1, -1), new Vector(0, -1, 0));
+        final Iterator<Vector> itr = Iterables.cycle(directions).iterator();
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                mSpotLight.setDirection(itr.next());
+            }
+        };
+
+        assertPass("Changing direction of light from -z to -y axis", () -> {
+            mSpotLight.setDirection(directions.get(0));
+        });
+        assertPass("running testColorChange()");
+
+    }
+
+    private void testSetPosition() {
+        final List<Vector> positions = Arrays.asList(new Vector(0, 0, -1), new Vector(0, -1, -1), new Vector(0, -1, 0));
+        final Iterator<Vector> itr = Iterables.cycle(positions).iterator();
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                mSpotLight.setPosition(itr.next());
+            }
+        };
+        assertPass("running testSetPosition()");
+    }
+
+    private void testSetInnerAngle() {
+        final List<Vector> positions = Arrays.asList(new Vector(0, 0, -1), new Vector(0, -1, -1), new Vector(0, -1, 0));
+        final Iterator<Vector> itr = Iterables.cycle(positions).iterator();
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                mSpotLight.setPosition(itr.next());
+            }
+        };
+        assertPass("running testSetInnerAngle()");
+
+    }
+
+    private void testSetOuterAngle() {
+        final List<Vector> positions = Arrays.asList(new Vector(0, 0, -1), new Vector(0, -1, -1), new Vector(0, -1, 0));
+        final Iterator<Vector> itr = Iterables.cycle(positions).iterator();
+        mMutableTestMethod = () -> {
+            if (mSpotLight != null) {
+                mSpotLight.setPosition(itr.next());
+            }
+        };
+        assertPass("running testSetInnerAngle()");
+    }
+
 }
