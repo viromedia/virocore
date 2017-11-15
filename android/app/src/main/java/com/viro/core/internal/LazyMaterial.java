81970d14d7f12cfa9c186a7fc05597ac22b837e8
Vik Advani <vadvani@viromedia.com>
2017-11-15 12:06:31 -0800

    Added proper copyright and rights to ViroCore.

--- android/app/src/main/java/com/viro/core/internal/LazyMaterial.java
+++ android/app/src/main/java/com/viro/core/internal/LazyMaterial.java
@@ -1,13 +1,9 @@
 /*
  * Copyright (c) 2017-present, ViroMedia, Inc.
  * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the  LICENSE file in the
- * root directory of this source tree. An additional grant  of patent rights can be found in
- * the PATENTS file in the same directory.
  */
-
 package com.viro.core.internal;
+
 /**
  The animation system requires lazy loading of materials from
  the bridge's material manager. To faciliate this we have this
