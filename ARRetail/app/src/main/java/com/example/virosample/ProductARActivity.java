875acde16d8a00ec24d93969dab2faa728ae6d21
Daniel Thian <daniel@viromedia.com>
2018-03-12 21:10:48 -0700

    Added Automatic Permission Checking for Screenshots in ARRetail

--- ARRetail/app/src/main/java/com/example/virosample/ProductARActivity.java
+++ ARRetail/app/src/main/java/com/example/virosample/ProductARActivity.java
@@ -16,17 +16,23 @@
  */
 package com.example.virosample;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.virosample.model.Product;
 import com.viro.core.ARAnchor;
@@ -156,6 +162,29 @@ public class ProductARActivity extends Activity {
         super.onDestroy();
     }
 
+    private void requestPermissions(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                0);
+    }
+
+    private static boolean hasAudioAndRecordingPermissions(Context context) {
+        boolean hasRecordPermissions = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
+        boolean hasExternalStoragePerm = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        return hasRecordPermissions && hasExternalStoragePerm;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (!hasAudioAndRecordingPermissions(ProductARActivity.this)) {
+            Toast toast = Toast.makeText(ProductARActivity.this, "User denied permissions", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
     private void displayScene() {
         // Create the ARScene within which to load our ProductAR Experience
         ARScene arScene = new ARScene();
@@ -206,6 +235,11 @@ public class ProductARActivity extends Activity {
         mCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!hasAudioAndRecordingPermissions(getBaseContext())){
+                    requestPermissions();
+                    return;
+                }
+
                 mViroView.getRecorder().takeScreenShotAsync("screenShot", true, new ViroMediaRecorder.ScreenshotFinishListener() {
                     @Override
                     public void onSuccess(Bitmap bitmap, String s) {
