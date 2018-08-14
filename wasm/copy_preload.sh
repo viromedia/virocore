44a8c6461ab61ecf60ef02b0f5e7bdbb61292aa8
Raj Advani <radvani@viromedia.com>
2018-08-14 01:34:11 -0400

    Make VROAtomic a public header

--- wasm/copy_preload.sh
+++ wasm/copy_preload.sh
@@ -4,5 +4,5 @@ echo Creating preload directory
 rm -rf products/preload && mkdir -p products/preload
 cp -r test/$1/preload/* products/preload
 cp -r preload/* products/preload
-echo Preload directory created
+echo Preload directory created, continuing to link...
 
