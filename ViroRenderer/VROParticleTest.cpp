384b2718fbfb8717c1f07107522df64bc5dbcf6a
Raj Advani <radvani@viromedia.com>
2018-11-13 13:05:51 -0500

    VIRO-4309: particle emitter blend mode testing
    Added extensive testing in ViroRenderer and ViroCore
    for particle emitter blend modes, specifically, Alpha,
    Add, and Screen. These three all work as expected.
    The other three (None, Multiply, Subtract) do not work
    well but these aren’t expected to work well with
    smoke-like particles.

--- ViroRenderer/VROParticleTest.cpp
+++ ViroRenderer/VROParticleTest.cpp
@@ -22,50 +22,88 @@ VROParticleTest::~VROParticleTest() {
 void VROParticleTest::build(std::shared_ptr<VRORenderer> renderer,
                             std::shared_ptr<VROFrameSynchronizer> frameSynchronizer,
                             std::shared_ptr<VRODriver> driver) {
-    _sceneController = std::make_shared<VROSceneController>();
+    _sceneController = std::make_shared<VROARSceneController>();
     std::shared_ptr<VROScene> scene = _sceneController->getScene();
+    std::shared_ptr<VROPortal> rootNode = scene->getRootNode();
     
-    std::shared_ptr<VRONode> rootNode = std::make_shared<VRONode>();
-    rootNode->setPosition({0, 0, 0});
-    scene->getRootNode()->addChildNode(rootNode);
+    std::shared_ptr<VROTexture> environment = VROTestUtil::loadRadianceHDRTexture("ibl_mans_outside");
+    rootNode->setBackgroundSphere(environment);
     
     std::shared_ptr<VRONode> particleNode = std::make_shared<VRONode>();
-    particleNode->setPosition({0, -10, -15});
+    particleNode->setPosition({0, -0.5, -1});
     particleNode->setTag("Particles");
     
-    std::shared_ptr<VROTexture> imgTexture = VROTestUtil::loadDiffuseTexture("cloud");
-    std::shared_ptr<VROSurface> surface = VROSurface::createSurface(1,1);
-    std::shared_ptr<VROParticleEmitter> particleEmitter = std::make_shared<VROParticleEmitter>(driver, surface);
+    // Particle surface
+    std::shared_ptr<VROSurface> surface = VROSurface::createSurface(1, 1);
+    std::shared_ptr<VROTexture> texture = VROTestUtil::loadDiffuseTexture("darkSmoke");
+    std::shared_ptr<VROMaterial> material = std::make_shared<VROMaterial>();
+    material->getDiffuse().setTexture(texture);
+    surface->setMaterials({ material });
     
-    // Vec of intervals to interpolate this modifier along.
-    std::vector<VROParticleModifier::VROModifierInterval> intervals;
-    VROParticleModifier::VROModifierInterval interval1;
-    interval1.startFactor = 0;
-    interval1.endFactor = 1000;
-    interval1.targetedValue = VROVector3f(0,0,0);
-    intervals.push_back(interval1);
+    // Core emitter settings
+    std::shared_ptr<VROParticleEmitter> emitter = std::make_shared<VROParticleEmitter>(driver, surface);
+    emitter->setParticleLifeTime({ 6000, 7000 });
+    emitter->setEmissionRatePerSecond({ 12, 18 });
+    emitter->setMaxParticles(100);
+    emitter->setDuration(1000);
+    emitter->setLoop(true);
+    emitter->setBlendMode(VROBlendMode::Add);
     
-    // Modifier's starting configuration. Provide different numbers to randomize.
-    VROVector3f sizeMinStart = VROVector3f(2,2,2);
-    VROVector3f sizeMaxStart = VROVector3f(2,2,2);
+    // Spawn volume
+    VROParticleSpawnVolume volume;
+    volume.shape = VROParticleSpawnVolume::Shape::Sphere;
+    volume.shapeParams = { 0.1 };
+    emitter->setParticleSpawnVolume(volume);
     
-    std::shared_ptr<VROParticleModifier> mod = std::make_shared<VROParticleModifier>(sizeMinStart,
-                                                                                     sizeMaxStart,
-                                                                                     VROParticleModifier::VROModifierFactor::Time,
-                                                                                     intervals);
-    // Finally set this modifier.
-    particleEmitter->setScaleModifier(mod);
-    particleNode->setParticleEmitter(particleEmitter);
+    // Scale modifier
+    std::vector<VROParticleModifier::VROModifierInterval> scaleIntervals;
+    VROParticleModifier::VROModifierInterval scaleInterval;
+    scaleInterval.startFactor = 0;
+    scaleInterval.endFactor = 1000;
+    scaleInterval.targetedValue = VROVector3f(1, 1, 1);
+    scaleIntervals.push_back(scaleInterval);
+    
+    VROVector3f scaleStartMin = { 0.3, 0.3, 0.3 };
+    VROVector3f scaleStartMax = { 0.3, 0.3, 0.3 };
+    std::shared_ptr<VROParticleModifier> scale = std::make_shared<VROParticleModifier>(scaleStartMin,
+                                                                                       scaleStartMax,
+                                                                                       VROParticleModifier::VROModifierFactor::Time,
+                                                                                       scaleIntervals);
+    emitter->setScaleModifier(scale);
+    
+    // Opacity modifier
+    std::vector<VROParticleModifier::VROModifierInterval> opacityIntervals;
+    VROParticleModifier::VROModifierInterval opacityIntervalA;
+    opacityIntervalA.startFactor = 0;
+    opacityIntervalA.endFactor = 1000;
+    opacityIntervalA.targetedValue = VROVector3f(1, 1, 1);
+    opacityIntervals.push_back(opacityIntervalA);
+    
+    VROParticleModifier::VROModifierInterval opacityIntervalB;
+    opacityIntervalB.startFactor = 2000;
+    opacityIntervalB.endFactor = 3000;
+    opacityIntervalB.targetedValue = VROVector3f(0, 0, 0);
+    opacityIntervals.push_back(opacityIntervalB);
+    
+    VROVector3f opacityStartMin = { 0, 0, 0 };
+    VROVector3f opacityStartMax = { 0, 0, 0 };
+    std::shared_ptr<VROParticleModifier> opacity = std::make_shared<VROParticleModifier>(opacityStartMin,
+                                                                                         opacityStartMax,
+                                                                                         VROParticleModifier::VROModifierFactor::Time,
+                                                                                         opacityIntervals);
+    emitter->setAlphaModifier(opacity);
+    
+    // Velocity modifier
+    std::vector<VROParticleModifier::VROModifierInterval> velocityIntervals;
+    VROVector3f velocityStartMin = {  0.05, 0.15, 0.01 };
+    VROVector3f velocityStartMax = { -0.05, 0.15, 0.01 };
+    std::shared_ptr<VROParticleModifier> velocity = std::make_shared<VROParticleModifier>(velocityStartMin,
+                                                                                          velocityStartMax,
+                                                                                          VROParticleModifier::VROModifierFactor::Time,
+                                                                                          velocityIntervals);
+    emitter->setVelocityModifier(velocity);
+    
+    particleNode->setParticleEmitter(emitter);
     rootNode->addChildNode(particleNode);
-    particleEmitter->setRun(true);
-    
-    /*
-     // Uncomment to test animating a particle emitter.
-     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-     VROTransaction::begin();
-     VROTransaction::setAnimationDuration(64);
-     particleNode->setRotationEulerY(96);
-     VROTransaction::commit();
-     });
-     */
+    emitter->setRun(true);
 }
--- android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroParticleEmitterTest.java
+++ android/releasetest/src/androidTest/java/com/viromedia/releasetest/tests/ViroParticleEmitterTest.java
@@ -11,6 +11,7 @@ package com.viromedia.releasetest.tests;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 
 import com.viro.core.Material;
 import com.viro.core.Node;
@@ -55,20 +56,13 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
 
     @Override
     void configureTestScene() {
-        if (mEmitter != null){
+        if (mEmitter != null) {
             mEmitter.dispose();
         }
-
-        mEmitter = createBaseEmitter();
-
-        // Prepare the node
         mEmitterNode = new Node();
-
-        // Attach the emitter to the node.
+        mEmitter = createBaseEmitter();
         mEmitterNode.setPosition(new Vector(0, -10, -15));
         mEmitterNode.setParticleEmitter(mEmitter);
-
-        // Attach the node to the scene.
         mScene.getRootNode().addChildNode(mEmitterNode);
 
         basicEmitterSetup();
@@ -77,7 +71,7 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
     /**
      * Detaches particle node from the scene and destroys it.
      */
-    private void cleanupTest(){
+    private void cleanupTest() {
         // Remove particle node from scene.
         mEmitterNode.removeFromParentNode();
         mEmitterNode.setParticleEmitter(null);
@@ -85,7 +79,7 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
         mEmitterNode.dispose();
     }
 
-    private void basicEmitterSetup(){
+    private void basicEmitterSetup() {
         mEmitter.setParticleLifetime(2000, 2000);
         mEmitter.setDuration(2000);
         mEmitter.setLoop(true);
@@ -96,6 +90,9 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
 
     @Test
     public void particleEmitterTests() {
+        runUITest(() -> blendModeTest(Material.BlendMode.ALPHA));
+        runUITest(() -> blendModeTest(Material.BlendMode.ADD));
+        runUITest(() -> blendModeTest(Material.BlendMode.SCREEN));
         runUITest(() -> singleParticleTest());
         runUITest(() -> emitterColorTest());
         runUITest(() -> emitterScaleTest());
@@ -124,7 +121,6 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
 
         assertPass("You should see the particle emit with red color and change to blue.");
     }
-
 
     public void emitterScaleTest() {
         Vector startSize = new Vector(1,1,0);
@@ -172,7 +168,6 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
         int colorStart = Color.RED;
         int colorEnd = Color.BLUE;
 
-
         ParticleEmitter.ParticleModifierFloat mod1
                 = new ParticleEmitter.ParticleModifierFloat(startRot, endRot);
         mod1.addInterval(1000, endRot);
@@ -195,6 +190,75 @@ public class ViroParticleEmitterTest extends ViroBaseTest {
         mEmitter.run();
 
         assertPass("Particles should change color, grow, fade out, rotate, non-uniformly.");
+    }
+
+    public void blendModeTest(Material.BlendMode blendMode) {
+        if (mEmitter != null) {
+            mEmitter.dispose();
+        }
+        if (mEmitterNode != null) {
+            mEmitterNode.removeFromParentNode();
+        }
+        Texture environment = Texture.loadRadianceHDRTexture(Uri.parse("file:///android_asset/ibl_mans_outside.hdr"));
+        mScene.setBackgroundTexture(environment);
+
+        mEmitterNode = new Node();
+        mEmitterNode.setPosition(new Vector(0, -0.5, -1));
+
+        // Particle surface
+        Quad quad = new Quad(1, 1);
+        final Bitmap result = this.getBitmapFromAssets(mActivity, "darkSmoke.png");
+
+        final Texture texture = new Texture(result, Texture.Format.RGBA8, true, true);
+        Material material = new Material();
+        material.setDiffuseTexture(texture);
+        quad.setMaterials(Arrays.asList(material));
+
+        // Core emitter settings
+        mEmitter = new ParticleEmitter(mViroView.getViroContext(), quad);
+        mEmitter.setParticleLifetime(6000, 7000);
+        mEmitter.setEmissionRatePerSecond(12, 18);
+        mEmitter.setMaxParticles(100);
+        mEmitter.setDuration(1000);
+        mEmitter.setLoop(true);
+        mEmitter.setBlendMode(blendMode);
+
+        // Spawn volume
+        ParticleEmitter.SpawnVolume volume = new ParticleEmitter.SpawnVolumeSphere(0.1f);
+        mEmitter.setSpawnVolume(volume, false);
+
+        // Scale modifier
+        ParticleEmitter.ParticleModifierVector scale = new ParticleEmitter.ParticleModifierVector(new Vector(0.3, 0.3, 0.3), new Vector(0.3, 0.3, 0.3));
+        scale.setFactor(ParticleEmitter.Factor.TIME);
+        scale.addInterval(1000, new Vector(1, 1, 1));
+        mEmitter.setScaleModifier(scale);
+
+        // Opacity modifier
+        ParticleEmitter.ParticleModifierFloat opacity = new ParticleEmitter.ParticleModifierFloat(0, 0);
+        opacity.setFactor(ParticleEmitter.Factor.TIME);
+        opacity.addInterval(1000, 1.0f);
+        opacity.addInterval(1000, 1.0f);
+        opacity.addInterval(1000, 0.0f);
+        mEmitter.setOpacityModifier(opacity);
+
+        // Velocity modifier
+        ParticleEmitter.ParticleModifierVector velocity = new ParticleEmitter.ParticleModifierVector(new Vector(0.05, 0.15, 0.01 ),
+                new Vector(-0.05, 0.15, 0.01));
+        mEmitter.setVelocityModifier(velocity);
+
+        mEmitterNode.setParticleEmitter(mEmitter);
+        mScene.getRootNode().addChildNode(mEmitterNode);
+        mEmitter.run();
+
+        if (blendMode == Material.BlendMode.ALPHA) {
+            assertPass("Smoke should fade in, be dark and opaque, then fade out");
+        }
+        else if (blendMode == Material.BlendMode.ADD) {
+            assertPass("Smoke should be white and should brighten the background");
+        }
+        else if (blendMode == Material.BlendMode.SCREEN) {
+            assertPass("Smoke should be white and should diminish the background");
+        }
     }
 
     public void multipleIntervalTest(){
